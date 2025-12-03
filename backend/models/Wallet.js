cbd068b6ab44bdfa4187049e654395bf8008e8de
Kacper <kacpergrzywacz2@gmail.com>
2025-12-03 22:16:20 +0100

    add: model Wallet.js

---
+++ backend/models/Wallet.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const walletSchema = new mongoose.Schema({
+	userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
+	balance: {
+		PLN: { type: Number, default: 0 },
+		USD: { type: Number, default: 0 },
+		EUR: { type: Number, default: 0 },
+	},
+});
+
+module.exports = mongoose.model("Wallet", walletSchema);
