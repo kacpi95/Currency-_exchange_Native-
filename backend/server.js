2f4fdbfdf60c1fd19f47f6c07f599908bb1d2a6b
Kacper <kacpergrzywacz2@gmail.com>
2025-12-04 22:15:31 +0100

    add: routs register to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,35 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+const User = require("./models/User");
+const Wallet = require("./models/Wallet");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+router.post("/register", async (req, res) => {
+	try {
+		const { email, password, name } = req.body;
+
+		const existing = await User.findOne({ email });
+		if (existing)
+			return res.status(400).json({ message: "Email already used" });
+
+		const passwordHash = await bcrypt.hash(password, 10);
+
+		const user = await User.create({
+			email,
+			passwordHash,
+			name,
+		});
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
 
 mongoose
 	.connect(process.env.MONGO_URL)
