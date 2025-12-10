cfe4784e9455b990ea89cfcdc5b4bb1859896eed
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-10 12:26:12 +0100

    add: Wallet.routes with imports

---
+++ backend/routes/Wallet.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const Wallet = require('../models/Wallet.model');
+const Transaction = require('../models/Transaction.model');
+
+
