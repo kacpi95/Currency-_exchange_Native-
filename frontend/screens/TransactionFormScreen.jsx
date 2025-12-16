6f540fe6bcb2b0622508daca22b03d9150d3e360
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-16 20:37:16 +0100

    add: UI to TransactionFormScreen

--- frontend/screens/TransactionFormScreen.jsx
+++ frontend/screens/TransactionFormScreen.jsx
@@ -1,7 +1,14 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
-import { Alert } from 'react-native';
+import {
+  Alert,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import TransactionApi from '../api/transaction';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function TransactionFormScreen({ route }) {
   const { token } = useContext(AuthContext);
@@ -33,3 +40,53 @@ export default function TransactionFormScreen({ route }) {
     }
   };
 }
+
+return (
+  <SafeAreaView style={styles.container}>
+    <Text style={styles.title}>Make Transaction</Text>
+
+    <Text style={styles.subtitle}>Type:</Text>
+    <TouchableOpacity
+      style={styles.button}
+      title='Buy'
+      onPress={() => setType('buy')}
+      color={type === 'buy' ? 'green' : 'grey'}
+    />
+    <TouchableOpacity
+      style={styles.button}
+      title='Sell'
+      onPress={() => setType('sell')}
+      color={type === 'sell' ? 'red' : 'grey'}
+    />
+
+    <Text style={styles.subtitle}>From Currency:</Text>
+    <TextInput
+      style={styles.input}
+      value={fromCurrency}
+      onChangeText={setFromCurrency}
+    />
+
+    <Text style={styles.subtitle}>To Currency:</Text>
+    <TextInput
+      style={styles.input}
+      value={toCurrency}
+      onChangeText={setToCurrency}
+    />
+
+    <Text style={styles.subtitle}>Amount:</Text>
+    <TextInput
+      style={styles.input}
+      value={amountFrom}
+      onChangeText={setAmountFrom}
+      keyboardType='numeric'
+    />
+
+    <TouchableOpacity
+      title={loading ? 'Processing...' : 'Submit'}
+      onPress={handleTransaction}
+      disabled={loading}
+    />
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({});
